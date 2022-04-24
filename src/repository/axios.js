ab5687b4f24e331ff7f8ba035fdbbaa08646ddd3
Igor Danilovski <igor_danilovski@hotmail.com>
2022-04-24 19:04:54 +0200

    .

--- src/repository/axios.js
+++ src/repository/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosConnectionInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://igor-emt-backend.herokuapp.com',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
